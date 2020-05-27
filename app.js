51967766711273d6fc562dccd60b76abeb00156a
Nebojsa Stojakovic <63195510+NebojsaStojakovic@users.noreply.github.com>
2020-05-27 12:25:03 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('video');
+const text = intro.querySelector('h1');
+
+const section = document.querySelector('section');
+const end = section.querySelector('h1');
+
+//scroolmagic
+const controller = new ScrollMagic.Controller();
+
+let scene = new ScrollMagic.Scene({
+  duration: 9000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+.addIndicators()
+.setPin(intro)
+.addTo(controller);
+
+let textAnim = TweenMax.fromTo(text, 3, {opacity: 1}, {opacity: 0});
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+.setTween(textAnim)
+.addTo(controller);
+//video animation
+let accelamount = 0.2;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on('update', e => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  video.currentTime = delay;
+}, 100);
